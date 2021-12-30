75b544b4c06c0c5a2497d1b716c34aec08f61e0f
Artur Propp <artur.propp@me.com>
2021-12-30 14:42:00 +0100

    docs: update README + example code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ansible-inventory-vagrant
-Dynamic Ansible inventory for Vagrant machines
+The Dynamic Ansible inventory for Vagrant machines using Python 3.
+
+Actually, there is no need for such dynamic inventory with Vagrant, as one can make use of the
+[Ansible Provision](https://www.vagrantup.com/docs/provisioning/ansible) from Vagrant that is going to generate a
+static inventory which itself is much faster than having to dynamically poll for the inventory.
+
+This repo is just for archiving purposes here ;-) as it was copied from
+[this gist](https://gist.github.com/lorin/4cae51e123b596d5c60d) and adapted for Python 3. The copied gist itself was
+adapted from Mark Mandel's implementation which was on the Ansible core git repository.
+
+In order to test this dynamic inventory make sure you have the requirements installed.
+
+```bash
+vagrant up # this also invokes the provisioner
+pip3 install -r inventory/requirements.txt # install requirements for the Python script vagrant.py
+ansible-inventory --list # make use of the dynamic inventory to list all hosts
+ansible-playbook playbook.yml # invoke the playbook using ansible directly
+vagrant provision # uses the static .vagrant/provisioners/ansible/inventory instead
+```
---
+++ inventory/vagrant.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Adapted from Mark Mandel's implementation
+import argparse
+import json
+import paramiko
+import subprocess
+import sys
+import io
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Vagrant inventory script")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--list', action='store_true')
+    group.add_argument('--host')
+    return parser.parse_args()
+
+
+def list_running_hosts():
+    cmd = "vagrant status --machine-readable"
+    status = subprocess.check_output(cmd.split(),text=True).rstrip()
+    hosts = []
+    for line in status.split('\n'):
+        (_, host, key, value) = line.split(',')[:4]
+        if key == 'state' and value == 'running':
+            hosts.append(host)
+    return hosts
+
+
+def get_host_details(host):
+    cmd = "vagrant ssh-config {}".format(host)
+    p = subprocess.run(cmd.split(), capture_output=True)
+    config = paramiko.SSHConfig()
+    config.parse(io.StringIO(p.stdout.decode('ascii')))
+    c = config.lookup(host)
+    return {'ansible_host': c['hostname'],
+            'ansible_port': c['port'],
+            'ansible_user': c['user'],
+            'ansible_private_key_file': c['identityfile'][0]}
+
+
+def main():
+    args = parse_args()
+    if args.list:
+        hosts = list_running_hosts()
+        json.dump({'vagrant': hosts}, sys.stdout)
+    else:
+        details = get_host_details(args.host)
+        json.dump(details, sys.stdout)
+
+if __name__ == '__main__':
+    main()
